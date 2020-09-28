2201dcd594763d10525a11094ba786e76770b192
mktcode <kontakt@markus-kottlaender.de>
2020-09-28 21:44:46 +0200

    removed console.log

--- plugins/markdown.js
+++ plugins/markdown.js
@@ -4,7 +4,6 @@ const hljs = require('highlight.js')
 export default ({ app }, inject) => {
   inject('markdown', markdown({
     highlight: (str, lang) => {
-      console.log(lang)
       if (lang && hljs.getLanguage(lang)) {
         try {
           return hljs.highlight(lang, str).value;
